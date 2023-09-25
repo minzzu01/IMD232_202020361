575e3ed05c8c02068f1c8cdfe290b4701a8b2663
minzzu01 <143991749+minzzu01@users.noreply.github.com>
2023-09-25 12:59:10 +0900

    02STEP

---
+++ src/w3/sketchs/sketch2/script/sketch.js
@@ -0,0 +1,48 @@
+let pos;
+let vel;
+let acc;
+let radius = 50;
+
+function setup() {
+  setCanvasContainer('canvas', 3, 2, true);
+  background(255);
+  pos = createVector(width / 2, height / 2);
+  vel = createVector(0, 0);
+  acc = createVector();
+  console.log(pos);
+  console.log(vel);
+  ellipse(pos.x, pos.y, 50);
+}
+
+function draw() {
+  background(255);
+  update();
+  infiniteEdge();
+  display();
+}
+
+function display() {
+  fill('red');
+  ellipse(pos.x, pos.y, 2 * radius);
+}
+
+function update() {
+  acc = p5.Vector.random2D();
+  acc.mult(0.5);
+  vel.add(acc);
+  vel.limit(5);
+  pos.add(vel);
+}
+
+function infiniteEdge() {
+  if (pos.x < 0) {
+    pos.x = width;
+  } else if (pos.x > width) {
+    pos.x = 0;
+  }
+  if (pos.y < 0) {
+    pos.y = height;
+  } else if (pos.y > height) {
+    pos.y = 0;
+  }
+}
