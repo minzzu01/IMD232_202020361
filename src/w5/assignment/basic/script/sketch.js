fa5adcaf498281f628652fcfa81fc675794a512a
minzzu01 <143991749+minzzu01@users.noreply.github.com>
2023-10-16 13:03:57 +0900

    10.16.최종

--- src/w5/assignment/basic/script/sketch.js
+++ src/w5/assignment/basic/script/sketch.js
@@ -1,14 +1,13 @@
 const cNum = 8;
 const rNum = 8;
-let gridC;
-let gridR;
+const cellSize = 50; // 각 셀의 크기
 let angleBegin = 0;
-let angleBeginVel;
+let angleBeginVel = 0.01; // 회전 속도
 let angleStep;
 
 function setup() {
-  setCanvasContainer('canvas', 1, 1, true); // 캔버스를 1:1의 크기로 만든다.
-
+  setCanvasContainer('canvas', 1, 1, true);
+  angleStep = TWO_PI / cNum;
   colorMode(HSL, 360, 100, 100, 100);
   background(360, 0, 100);
 }
@@ -18,9 +17,14 @@ function draw() {
 
   for (let r = 0; r < rNum; r++) {
     for (let c = 0; c < cNum; c++) {
+      const x = c * cellSize + cellSize / 2;
+      const y = r * cellSize + cellSize / 2;
+
       push();
-      translate();
-      rotate();
+      translate(x, y); // 셀의 중심으로 이동
+      rotate(angleBegin + c * angleStep); // 회전 각도 적용
+      fill(200, 80, 70); // 예시로 사용할 노란색 셀
+      ellipse(0, 0, cellSize, cellSize); // ellipse로 변경
       pop();
     }
   }
