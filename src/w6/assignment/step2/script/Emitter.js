cee0c714530962decbe8dd191a0775139bde9e0f
minzzu01 <143991749+minzzu01@users.noreply.github.com>
2023-10-23 13:21:56 +0900

    1023120

--- src/w6/assignment/step2/script/Emitter.js
+++ src/w6/assignment/step2/script/Emitter.js
@@ -26,23 +26,31 @@ class Emitter {
     this.hasCreatedParticle = true;
   }
 
-  // 
+  //
   applyGravity(gravity) {
+    // particle 배열 반복, each = 배열 내 파티클
     this.particles.forEach((each) => {
+      // gravity 벡터를 각 파티클 질량[mass] 곱한 값을 scaleG에 저장
       const scaledG = p5.Vector.mult(gravity, each.mass);
+      // 각 입자에 새로 계산된 중력(scaledG) 적용
       each.applyForce(scaledG);
     });
   }
 
   update() {
+    // 파티클 배열 역순으로 반복
     for (let i = this.particles.length - 1; i >= 0; i--) {
+      // this.particle에 대해 update함수 호출, 파티클 상태 업데이트
       this.particles[i].update();
+      // 파티클 수명 다해 소멸 > 해당 파티클 배열에서 제거
       if (this.particles[i].isDead()) {
+        // splice 함수: 배열에서 해당 인덱스 파티클 제거
         this.particles.splice(i, 1);
       }
     }
   }
 
+  // forEach() 함수: particle 배열에 있는 각 파티클에 대한 반복 진행
   display() {
     this.particles.forEach((each) => {
       each.display();
