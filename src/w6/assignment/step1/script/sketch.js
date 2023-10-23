63f55eed0dfa3c7c65797e4bc4e027efa4363954
minzzu01 <143991749+minzzu01@users.noreply.github.com>
2023-10-23 13:38:45 +0900

    !!

--- src/w6/assignment/step1/script/sketch.js
+++ src/w6/assignment/step1/script/sketch.js
@@ -3,10 +3,10 @@ let gravity;
 
 function setup() {
   setCanvasContainer('canvas', 2, 1, true);
-  rectMode(CENTER);
+  rectMode(CENTER); //rect 위치
   colorMode(HSB, 360, 100, 100, 100);
   emitter = new Emitter(width / 2, 0); // 시작하는 위치를 변경
-  gravity = createVector(0, 0.1); // 중력을 느리게 설정
+  gravity = createVector(0, 0.1); // 중력을 느리게
   background(360, 0, 100);
 }
 
